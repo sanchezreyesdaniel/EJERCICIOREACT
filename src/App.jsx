103395bd7cb94a84ccb830b04f01d05a6f4f1553
sanchezreyesdaniel <sanchezreyesdaniel@fpllefia.com>
2024-02-19 20:15:31 +0100

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='p-5 flex flex-col items-center'>
+      <div>
+      <h1>Tailwind</h1>
+      <img width={100} src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/320px-Tailwind_CSS_Logo.svg.png" alt="" />
+      </div>
+      <div>
+      <h1>React</h1>
+      <img width={200} src="https://www.logo.wine/a/logo/React_(web_framework)/React_(web_framework)-Logo.wine.svg" alt="" />
+      </div>
+      <div>
+        <h1>Vite</h1>
+        <img width={100} src="https://seeklogo.com/images/V/vite-logo-BFD4283991-seeklogo.com.png" alt="" />
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
